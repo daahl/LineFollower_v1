3adcc37fa8bd6d0269bd3c0d77107fa66b9600e2
daahl <ma.dah@outlook.com>
2025-03-08 09:52:35 +0100

    Motors are working :)

--- LineFollowerBot/src/defines.h
+++ LineFollowerBot/src/defines.h
@@ -1,2 +1,14 @@
+// PCB LEDs
 #define LED1 32
 #define LED2 33
+
+// Motor driver
+#define AIN1    17
+#define AIN2    18
+#define PWMA    19
+#define LOFFSET -1
+#define BIN1    4
+#define BIN2    2
+#define PWMB    15
+#define ROFFSET -1
+#define STBY    27
--- LineFollowerBot/src/main.cpp
+++ LineFollowerBot/src/main.cpp
@@ -1,5 +1,35 @@
 #include <Arduino.h>
 #include "defines.h"
+#include "SparkFun_TB6612.h"
+
+// Motor setup
+Motor motorLL = Motor(AIN1, AIN2, PWMA, LOFFSET, STBY);
+Motor motorRR = Motor(BIN1, BIN2, PWMB, ROFFSET, STBY);
+int speed = 0;
+int turn = 0;
+
+void testMotors() {
+  motorLL.drive(255);
+  motorRR.drive(255);
+  delay(1000);
+  motorLL.drive(-255);
+  motorRR.drive(-255);
+  delay(1000);
+  motorLL.brake();
+  motorRR.brake();
+  delay(1000);
+}
+
+void driveMotors(){
+  motorLL.drive(speed + turn);
+  motorRR.drive(speed - turn);
+}
+
+void initLEDS() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+}
 
 void blinkLEDS() {
   digitalWrite(LED_BUILTIN, HIGH);
@@ -14,14 +44,14 @@ void blinkLEDS() {
 
 void setup() {
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
+  initLEDS();
 
 }
 
 void loop() {
   
   blinkLEDS();
+
+  testMotors();
 
 }
